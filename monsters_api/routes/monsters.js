a82c1db94c8619f8af2a654ee000be0274ad4859
jahanner7@gmail.com <jahanner7@gmail.com>
2018-02-16 16:44:58 -0600

    Added habitats route

--- monsters_api/routes/monsters.js
+++ monsters_api/routes/monsters.js
@@ -4,10 +4,11 @@ const pool = require("../db");
 const router = Router();
 
 router.get("/", (request, response, next) => {
+  //'/monsters' is already set as default in app.js
   pool.query("SELECT * FROM monsters ORDER BY id ASC", (err, res) => {
     if (err) return next(err);
 
-    response.json(res.rows);
+    response.json(res.rows); //give a response of the rows in the result
   });
 });
 
@@ -28,9 +29,43 @@ router.post("/", (request, response, next) => {
     (err, res) => {
       if (err) return next(err);
 
-      response.redirect("/monsters");
+      response.redirect("/monsters"); //allows redirect so you don't have to set another Get request
     }
   );
+});
+
+router.put("/:id", (request, response, next) => {
+  //PUT allows updating of data; NEXT for handling errors
+  console.log(request.body);
+  const { id } = request.params;
+  const keys = ["name", "personality"];
+  const fields = [];
+
+  keys.forEach(key => {
+    if (request.body[key]) fields.push(key);
+  });
+
+  fields.forEach((field, index) => {
+    pool.query(
+      `UPDATE monsters SET ${field}=$1 WHERE id=$2`,
+      [request.body[field], id],
+      (err, res) => {
+        if (err) return next(err);
+
+        if (index === fields.length - 1) response.redirect(303, "/monsters");
+      }
+    );
+  });
+});
+
+router.delete("/:id", (request, response, next) => {
+  const { id } = request.params;
+
+  pool.query("DELETE FROM monsters WHERE id=($1)", [id], (err, res) => {
+    if (err) return next(err);
+
+    response.redirect(303, "/monsters");
+  });
 });
 
 module.exports = router;
